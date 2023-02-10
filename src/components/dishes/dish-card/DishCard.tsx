2647e9434bd8dcfd18941aff379ea122b0c567fa
hejs22 <hcywka@gmail.com>
2023-02-10 14:18:22 +0100

    T-5: UI overhaul of dish card, added rating components

--- src/components/dishes/dish-card/DishCard.tsx
+++ src/components/dishes/dish-card/DishCard.tsx
@@ -1,7 +1,8 @@
-import { Card, CardMedia, Typography } from '@mui/material';
+import { Box, Button, Card, CardMedia, Typography } from '@mui/material';
 import './DishCard.scss';
 import IngredientsList from '../../ingredients/ingredients-list/IngredientsList';
 import { Dish } from '../../../interfaces/Dish';
+import DishRating from '../dish-rating/DishRating';
 
 interface DishCardProps {
   dish: Dish;
@@ -17,11 +18,23 @@ const DishCard = ({ dish, className }: DishCardProps) => {
         alt={dish.name}
         image={dish.img}></CardMedia>
       <Typography className="dish-name">{dish.name}</Typography>
+      <Box className="ratings-container">
+        <DishRating className="dish-rating" type={'personal'} />
+        <DishRating className="dish-rating" type={'global'} />
+      </Box>
       <IngredientsList
         className="ingredients-list"
         ingredients={dish.ingredients}
         amountLimit={5}
       />
+      <Button
+        sx={{
+          textTransform: 'none'
+        }}
+        variant="contained"
+        className="show-details-button">
+        Show recipe
+      </Button>
     </Card>
   );
 };
