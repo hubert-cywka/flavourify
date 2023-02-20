05d4f851b969b112b1192dd5aaa23f0179d44c45
hejs22 <hcywka@gmail.com>
2023-02-20 17:26:51 +0100

    T-16: Tags management panel

--- src/services/TagsService.tsx
+++ src/services/TagsService.tsx
@@ -1,7 +1,22 @@
-import { Tag } from '../interfaces/Tag';
+import { Tag, TagType } from '../interfaces/Tag';
 import { apiClient } from './ApiClient';
 
 export const getTags = async (): Promise<Tag[]> => {
   const { data } = await apiClient.get('/tags');
+  return data;
+};
+
+export const createTag = async (tagName: string, tagType: TagType): Promise<Tag> => {
+  const { data } = await apiClient.post<Tag>('/tags', { name: tagName, type: tagType });
+  return data;
+};
+
+export const deleteTag = async (tagId: number) => {
+  const { data } = await apiClient.delete<Tag>(`/tags/${tagId}`);
+  return data;
+};
+
+export const updateTag = async (tag: Tag): Promise<Tag> => {
+  const { data } = await apiClient.put<Tag>(`/tags/${tag.id}`, tag);
   return data;
 };
