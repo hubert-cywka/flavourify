581c0855e0cc8f3ce8f374a1551e018ea2952e3b
Hubert Cywka <hcywka@gmail.com>
2023-04-05 09:54:24 +0200

    T-43: Users management panel

---
+++ src/constants/UserConstants.tsx
@@ -0,0 +1,8 @@
+export const USER_DELETE_SUCCESS = 'User deleted successfully.';
+export const USER_DELETE_ERROR = 'Failed to delete user.';
+export const USER_ROLE_CHANGE_SUCCESS = 'User role changed successfully.';
+export const USER_ROLE_CHANGE_ERROR = 'Failed to change user role.';
+export const USER_EDIT_INFO = `Select user and remove one's account, give admin permissions or degrade to normal user, without ability to change tags, dishes and users.`;
+export const USER_EDIT_WARNING = `For security reasons, your account is not displayed here.`;
+export const USERS_NOT_FOUND_ERROR = `Seems like you are the only user. But don't worry, others are queueing to sign up, for sure!`;
+export const USERS_NOT_FOUND_IMAGE = '/users-not-found.svg';
--- src/services/UserService.tsx
+++ src/services/UserService.tsx
@@ -6,3 +6,17 @@ export const createUser = async (userData: SignUpRequest) => {
   const { data } = await apiClient.post<User>(`/auth/signup`, userData);
   return data;
 };
+
+export const getUsers = async () => {
+  const { data } = await apiClient.get<User[]>('/users');
+  return data;
+};
+
+export const deleteUser = async (userId: number) => {
+  await apiClient.delete(`/users/${userId}`);
+};
+
+export const updateUserRole = async (userId: number, role: string) => {
+  const { data } = await apiClient.patch(`/users/${userId}`, role);
+  return data;
+};
