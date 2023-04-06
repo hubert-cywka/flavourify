67704615ff01e637a47f718fbeca0b9063e3c448
Hubert Cywka <hcywka@gmail.com>
2023-04-06 09:48:15 +0200

    Set up jest and tests workflow

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['./src'],
+  transform: { '\\.ts$': ['ts-jest'] },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  globals: {
+    'ts-jest': {
+      tsConfig: {
+        allowJs: true
+      }
+    }
+  }
+};
---
+++ src/app.test.tsx
@@ -0,0 +1,6 @@
+import '@testing-library/jest-dom';
+describe('Testing <App /> component', () => {
+  test('1 + 1 = 2', async () => {
+    expect(1 + 2).toBe(3);
+  });
+});
