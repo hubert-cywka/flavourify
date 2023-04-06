3ddddd4890b2c80e95ede52a84ed70faf06d4321
Hubert Cywka <hcywka@gmail.com>
2023-04-06 09:53:57 +0200

    test

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -3,4 +3,8 @@ describe('Testing <App /> component', () => {
   test('1 + 1 = 2', async () => {
     expect(1 + 2).toBe(3);
   });
+
+  test('1 + 1 = 3', async () => {
+    expect(1 + 3).toBe(4);
+  });
 });
