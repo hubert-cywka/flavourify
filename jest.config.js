d390ce07067d0bfc850348aa0a27af3afac76b4d
Hubert Cywka <hcywka@gmail.com>
2023-04-06 10:41:15 +0200

    Change coverage command

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['./src'],
+  coverageReporters: ['cobertura', 'html', 'text', 'text-summary'],
   transform: { '\\.ts$': ['ts-jest'] },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
