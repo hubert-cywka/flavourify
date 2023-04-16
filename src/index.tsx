ce9b7aef1bad10079dee66869fe829ca19df053c
Hubert Cywka <hcywka@gmail.com>
2023-04-16 13:39:12 +0200

    T-53: Fix problems with dish card back side and swiper

--- src/index.tsx
+++ src/index.tsx
@@ -4,18 +4,10 @@ import './index.scss';
 import App from './App';
 import { setupInterceptors } from './services/ApiClient';
 import { AlertProvider } from './AlertProvider';
-import {
-  setCustomViewportHeightVariable,
-  setCustomViewportSizeVariableUpdater,
-  setCustomViewportWidthVariable
-} from './utility/viewportSizeVariable';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 setupInterceptors();
-setCustomViewportWidthVariable();
-setCustomViewportHeightVariable();
-setCustomViewportSizeVariableUpdater();
 
 root.render(
   <React.StrictMode>
