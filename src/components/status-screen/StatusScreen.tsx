8c5b23a87ac631cdaedb1b2230f37ed2bcfed592
Hubert Cywka <hcywka@gmail.com>
2023-05-09 00:04:00 +0200

    T69.3: Fix DishCard

--- src/components/status-screen/StatusScreen.tsx
+++ src/components/status-screen/StatusScreen.tsx
@@ -1,7 +1,5 @@
-import { Box, Button, Modal, Typography } from '@mui/material';
+import { Box, Button, Grow, Modal, Typography } from '@mui/material';
 import './StatusScreen.scss';
-import { wobblyAppearAnimation } from '../../constants/AnimationConfigs';
-import AnimatePresence from '../animate-presence/AnimatePresence';
 
 interface StatusScreenProps {
   header: string;
@@ -29,34 +27,33 @@ const StatusScreen = ({
   return (
     <Modal sx={{ color: 'text.primary' }} className="status-screen-popup" open={open}>
       <Box>
-        <AnimatePresence
-          isVisible={open}
-          animation={wobblyAppearAnimation}
-          className="status-screen-container">
-          <img className="status-screen-image" src={imgSource} />
-          <Typography
-            className="status-screen-header"
-            sx={{ color: status === 'success' ? 'accent.success' : 'accent.error' }}>
-            {header}
-          </Typography>
-          <Typography className="status-screen-info">{caption}</Typography>
-          <Box className="status-screen-buttons">
-            {secondButtonOnClick && (
+        <Grow in={open} className="status-screen-container">
+          <>
+            <img className="status-screen-image" src={imgSource} />
+            <Typography
+              className="status-screen-header"
+              sx={{ color: status === 'success' ? 'accent.success' : 'accent.error' }}>
+              {header}
+            </Typography>
+            <Typography className="status-screen-info">{caption}</Typography>
+            <Box className="status-screen-buttons">
+              {secondButtonOnClick && (
+                <Button
+                  variant={'accentContained'}
+                  className="status-screen-secondary-button"
+                  onClick={secondButtonOnClick}>
+                  {secondButtonText}
+                </Button>
+              )}
               <Button
-                variant={'accentContained'}
-                className="status-screen-secondary-button"
-                onClick={secondButtonOnClick}>
-                {secondButtonText}
+                variant={status === 'success' ? 'successContained' : 'errorContained'}
+                className="status-screen-primary-button"
+                onClick={close}>
+                {buttonText ? buttonText : 'OK'}
               </Button>
-            )}
-            <Button
-              variant={status === 'success' ? 'successContained' : 'errorContained'}
-              className="status-screen-primary-button"
-              onClick={close}>
-              {buttonText ? buttonText : 'OK'}
-            </Button>
-          </Box>
-        </AnimatePresence>
+            </Box>
+          </>
+        </Grow>
       </Box>
     </Modal>
   );
